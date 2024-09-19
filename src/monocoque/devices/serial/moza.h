f636b239be937e28b38ec7a23a496414c7209d00
Paul Dino Jones <paul@spacefreak18.xyz>
2024-09-19 17:30:59 -0400

    moza support merge

---
+++ src/monocoque/devices/serial/moza.h
@@ -0,0 +1,10 @@
+#ifndef _MOZA_H
+#define _MOZA_H
+
+#include "../serialdevice.h"
+
+int moza_update(SerialDevice* serialdevice, unsigned short maxrpm, unsigned short rpm);
+int moza_init(SerialDevice* serialdevice);
+int moza_free(SerialDevice* serialdevice);
+
+#endif
