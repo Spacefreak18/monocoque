46cb401e6b95d45fa6c96bc22e1cc846ca92bc4f
Paul Dino Jones <paul@spacefreak18.xyz>
2025-03-25 10:09:39 -0400

    Always write to led

--- src/monocoque/devices/usb/wheels/cammusc12.c
+++ src/monocoque/devices/usb/wheels/cammusc12.c
@@ -206,11 +206,8 @@ int cammusc12_customled_update(USBDevice* usbdevice, SimData* simdata)
 
         if (usbdevice->handle)
         {
-            if(red != 0x00 || green != 0x00 || blue !=  0x00)
-            {
-                res = hid_write(usbdevice->handle, bytes, cammusc12_hidupdate_buf_size);
-                slogt("writing bytes x%02xx%02xx%02xx%02xx%02xx%02xx%02x from red %i green %i blue %i", bytes[0], bytes[1], bytes[2], bytes[3], bytes[4], bytes[5], bytes[6], red, green, blue);
-            }
+            res = hid_write(usbdevice->handle, bytes, cammusc12_hidupdate_buf_size);
+            slogt("writing bytes x%02xx%02xx%02xx%02xx%02xx%02xx%02x from red %i green %i blue %i", bytes[0], bytes[1], bytes[2], bytes[3], bytes[4], bytes[5], bytes[6], red, green, blue);
         }
         else
         {
